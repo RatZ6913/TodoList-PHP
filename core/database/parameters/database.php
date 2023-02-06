472d81c58db4d26d311feef016ea421d67985f9c
Pheak Rattana <rattana.pheak.rp@gmail.com>
2023-02-06 23:27:32 +0100

    Update
    Update

--- core/database/parameters/database.php
+++ core/database/parameters/database.php
@@ -3,8 +3,7 @@
 const DB_HOST = "127.0.0.1";
 const DB_PORT = "3306";
 const DB_DATABASE = "exercice_cours";
-// const DB_NAME = "root"; // Greta
-// const DB_PWD = ""; // Greta
+
 try {
   $pdo = new PDO(
     'mysql:host=' . DB_HOST . ';port=' . DB_PORT . ';dbname=' . DB_DATABASE,
@@ -16,14 +15,8 @@ try {
       PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8; USE exercice_cours'
     ]
   );
-  // echo "Réussi";
 } catch (PDOException $e) {
   throw new Exception($e->getMessage());
 }
-
 return $pdo;
 
-
-
-// cd '270.1.23 TP 2'
-// DB_NAME='' DB_PWD='' php -S localhost:3000 
